c2668de3e9f761f5f077c504d670b457a2945bc6
doGregor <gregor.donabauer@gmx.de>
2024-04-04 12:21:23 +0000

    add BERT finetuning baseline

--- baselines/baseline_bert.py
+++ baselines/baseline_bert.py
@@ -1,49 +1,103 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from transformers import TrainingArguments, Trainer
+import json
+import logging
+from os.path import join as pj
+import torch
 import numpy as np
-import evaluate
-from baselines_data_preparation import *
+from datasets import load_metric
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
 
 
-tokenizer = AutoTokenizer.from_pretrained("Twitter/twhin-bert-base")
-model = AutoModelForSequenceClassification.from_pretrained("Twitter/twhin-bert-base",
-                                                           num_labels=2)
+def get_metrics():
+    metric_accuracy = load_metric("accuracy")
+    metric_f1 = load_metric("f1")
 
-training_args = TrainingArguments(output_dir="test_trainer",
-                                  num_train_epochs=3,
-                                  per_device_train_batch_size=16,
-                                  evaluation_strategy="epoch")
-metric = evaluate.load("accuracy")
+    def compute_metric_search(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric_f1.compute(predictions=predictions, references=labels, average='micro')
 
-
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
-
-
-positive_tweets, negative_tweets = load_twitter_corpus()
-df = samples_to_df(positive_tweets, negative_tweets)
-dataset_train, dataset_test = dataset_from_df(df)
+    def compute_metric_all(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return {
+            'f1': metric_f1.compute(predictions=predictions, references=labels, average='micro')['f1'],
+            'f1_macro': metric_f1.compute(predictions=predictions, references=labels, average='macro')['f1'],
+            'accuracy': metric_accuracy.compute(predictions=predictions, references=labels)['accuracy']
+        }
+    return compute_metric_search, compute_metric_all
 
 
-def tokenize_function(examples):
-    return tokenizer(examples["text"],
-                     max_length=128,
-                     padding="max_length",
-                     truncation=True)
+def model_finetuning(train_data, eval_data, test_data, output_dir, label2id, seq_length=128, eval_steps=500, model_name='bert-base-cased', random_seed=42):
+    print('starting finetuning...')
+    id2label = {v: k for k, v in label2id.items()}
+    num_labels = len(list(id2label.keys()))
 
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_name,
+        num_labels=num_labels,
+        id2label=id2label,
+        label2id=label2id
+        )
+    tokenized_train = train_data.map(
+        lambda x: tokenizer(x["text"], padding="max_length", truncation=True, max_length=seq_length),
+        batched=True)
+    tokenized_eval = eval_data.map(
+        lambda x: tokenizer(x["text"], padding="max_length", truncation=True, max_length=seq_length),
+        batched=True)
 
-dataset_train = dataset_train.map(tokenize_function, batched=True)
-dataset_test = dataset_test.map(tokenize_function, batched=True)
+    # setup metrics
+    compute_metric_search, compute_metric_all = get_metrics()
 
+    # setup trainer
+    trainer = Trainer(
+        model=model,
+        args=TrainingArguments(
+            output_dir=output_dir,
+            evaluation_strategy="steps",
+            eval_steps=eval_steps,
+            seed=random_seed
+        ),
+        train_dataset=tokenized_train,
+        eval_dataset=tokenized_eval,
+        compute_metrics=compute_metric_search,
+        model_init=lambda x: AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            num_labels=num_labels,
+            return_dict=True,
+            id2label=id2label,
+            label2id=label2id
+        )
+    )
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=dataset_train,
-    eval_dataset=dataset_test,
-    compute_metrics=compute_metrics,
-)
+    trainer.train()
+    trainer.save_model(pj(output_dir, 'best_model'))
+    best_model_path = pj(output_dir, 'best_model')
+    
+    tokenized_test = test_data.map(
+        lambda x: tokenizer(x["text"], padding="max_length", truncation=True, max_length=seq_length),
+        batched=True)
 
-trainer.train()
+    # testing
+    model = AutoModelForSequenceClassification.from_pretrained(
+        best_model_path,
+        num_labels=num_labels,
+        id2label=id2label,
+        label2id=label2id
+    )
+    trainer = Trainer(
+        model=model,
+        args=TrainingArguments(
+            output_dir=output_dir,
+            evaluation_strategy="no",
+            seed=random_seed
+        ),
+        train_dataset=tokenized_train,
+        eval_dataset=tokenized_test,
+        compute_metrics=compute_metric_all,
+    )
+    summary_file = pj(output_dir, 'metric_summary.json')
+    result = {f'test/{k}': v for k, v in trainer.evaluate().items()}
+    logging.info(json.dumps(result, indent=4))
+    with open(summary_file, 'w') as f:
+        json.dump(result, f)
---
+++ baselines/load_datasets.py
@@ -0,0 +1,129 @@
+import nltk
+import ssl
+from nltk.corpus import twitter_samples
+
+
+def download_twitter_corpus():
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+    nltk.download('twitter_samples')
+
+
+def load_twitter_corpus():
+    try:
+        positive_tweets = twitter_samples.strings('positive_tweets.json')
+        negative_tweets = twitter_samples.strings('negative_tweets.json')
+    except:
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context
+        except AttributeError:
+            pass
+        else:
+            ssl._create_default_https_context = _create_unverified_https_context
+        nltk.download('twitter_samples')
+        positive_tweets = twitter_samples.strings('positive_tweets.json')
+        negative_tweets = twitter_samples.strings('negative_tweets.json')
+    return positive_tweets, negative_tweets
+
+
+def load_mr_corpus():
+    positive_samples = []
+    negative_samples = []
+    with open('../raw_data/MR/rt-polarity.pos', 'rb') as input_data:
+        for line in input_data.readlines():
+            if len(line.strip()) > 0:
+                try:
+                    positive_samples.append(line.decode("utf-8").strip().lower())
+                except:
+                    pass
+    with open('../raw_data/MR/rt-polarity.neg', 'rb') as input_data:
+        for line in input_data.readlines():
+            if len(line.strip()) > 0:
+                try:
+                    negative_samples.append(line.decode("utf-8").strip().lower())
+                except:
+                    pass
+    return positive_samples, negative_samples
+
+
+def load_snippets_corpus():
+    label_sample_dict = {
+        'business': [],
+        'computers': [],
+        'culture-arts-entertainment': [],
+        'education-science': [],
+        'engineering': [],
+        'health': [],
+        'politics-society': [],
+        'sports': []
+    }
+    all_samples = []
+    with open('../raw_data/Snippets/train.txt', 'r') as input_data:
+        for line in input_data.readlines():
+            if len(line.strip()) > 0:
+                try:
+                    all_samples.append(line.strip().lower())
+                except:
+                    pass
+    with open('../raw_data/Snippets/test.txt', 'r') as input_data:
+        for line in input_data.readlines():
+            if len(line.strip()) > 0:
+                try:
+                    all_samples.append(line.strip().lower())
+                except:
+                    pass
+    all_samples = list(set(all_samples))
+    for sample in all_samples:
+        label = sample.split()[-1]
+        label_sample_dict[label].append(' '.join(sample.split()[:-1]))
+
+    return (label_sample_dict['business'], label_sample_dict['computers'], label_sample_dict['culture-arts-entertainment'],
+            label_sample_dict['education-science'], label_sample_dict['engineering'], label_sample_dict['health'],
+            label_sample_dict['politics-society'], label_sample_dict['sports'])
+
+
+def load_tagmynews_corpus():
+    label_sample_dict = {
+        'health': [],
+        'business': [],
+        'world': [],
+        'us': [],
+        'sport': [],
+        'entertainment': [],
+        'sci_tech': []
+    }
+    all_samples = []
+    all_labels = []
+    with open('../raw_data/TagMyNews/tagmynews.txt', 'r') as input_data:
+        data = input_data.readlines()
+    data = [list(filter(None, sublist.split('\n'))) for sublist in ''.join(data).split('\n\n')]
+    for sample in data:
+        if len(sample) != 7:
+            continue
+        all_samples.append(sample[0]) # + '. ' + sample[1])
+        all_labels.append(sample[-1])
+    for idx, sample in enumerate(all_samples):
+        label = all_labels[idx]
+        label_sample_dict[label].append(sample.lower())
+
+    return (label_sample_dict['health'], label_sample_dict['business'], label_sample_dict['world'],
+            label_sample_dict['us'], label_sample_dict['sport'], label_sample_dict['entertainment'],
+            label_sample_dict['sci_tech'])
+
+
+if __name__ == '__main__':
+    # positive_tweets, negative_tweets = load_twitter_corpus()
+    # positive_samples, negative_samples = load_mr_corpus()
+    # samples = load_snippets_corpus()
+    samples = load_tagmynews_corpus()
+    for s in samples:
+        print(len(s), s[0])
+    """
+    print(len(positive_samples), len(negative_samples))
+    for idx in range(10):
+        print(positive_samples[idx], '###', negative_samples[idx])
+    """
