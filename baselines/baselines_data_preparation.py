98b36298e41f09c8f95ad51f394fd7cb5b4c6360
Gregor Donabauer <gregor.donabauer@gmx.de>
2024-04-04 09:36:10 +0200

    Update baselines_data_preparation.py

--- baselines/baselines_data_preparation.py
+++ baselines/baselines_data_preparation.py
@@ -1,49 +1,64 @@
 import sys
-
-from nltk.corpus import twitter_samples
+from load_datasets import *
 import random
-import pandas as pd
 from datasets import Dataset
+from random import shuffle
+import pyarrow as pa
 
 
-def load_twitter_corpus():
-    positive_tweets = twitter_samples.strings('positive_tweets.json')
-    negative_tweets = twitter_samples.strings('negative_tweets.json')
-    return positive_tweets, negative_tweets
+def load_train_val_test_baseline(dataset, num_per_class=20):
+    if dataset == 'twitter':
+        samples = load_twitter_corpus()
+    elif dataset == 'mr':
+        samples = load_mr_corpus()
+    elif dataset == 'snippets':
+        samples = load_snippets_corpus()
+    elif dataset == 'tag_my_news':
+        samples = load_tagmynews_corpus()
+    train_samples = []
+    train_labels = []
+    val_samples = []
+    val_labels = []
+    test_samples = []
+    test_labels = []
+    for class_idx, class_samples in enumerate(samples):
+        shuffle(class_samples)
+        train_samples += class_samples[:num_per_class]
+        train_labels += [class_idx] * num_per_class
+        val_samples += class_samples[num_per_class:num_per_class + num_per_class]
+        val_labels += [class_idx] * num_per_class
+        test_samples += class_samples[num_per_class + num_per_class:]
+        test_labels += [class_idx] * (len(class_samples) - 2 * num_per_class)
+    c = list(zip(train_samples, train_labels))
+    random.shuffle(c)
+    train_samples, train_labels = zip(*c)
+    c = list(zip(val_samples, val_labels))
+    random.shuffle(c)
+    val_samples, val_labels = zip(*c)
+    c = list(zip(test_samples, test_labels))
+    random.shuffle(c)
+    test_samples, test_labels = zip(*c)
+    return train_samples, train_labels, val_samples, val_labels, test_samples, test_labels
 
 
-def samples_to_df(pos_samples, neg_samples, number_per_class=40):
-    pos_samples = [s for s in pos_samples if len(s.strip()) > 0]
-    neg_samples = [s for s in neg_samples if len(s.strip()) > 0]
-
-    pos_samples = pos_samples[:number_per_class]
-    neg_samples = neg_samples[:number_per_class]
-
-    pos_labels = [1] * len(pos_samples)
-    neg_labels = [0] * len(neg_samples)
-
-    text = pos_samples + neg_samples
-    labels = pos_labels + neg_labels
-    zipped_lists = list(zip(text, labels))
-    random.shuffle(zipped_lists)
-    text, labels = zip(*zipped_lists)
-
-    df = pd.DataFrame(list(zip(text, labels)),
-                      columns=['text', 'label'])
-    return df
-
-
-def dataset_from_df(df):
-    dataset = Dataset.from_pandas(df=df)
-    dataset = dataset.train_test_split(test_size=0.5)
-    dataset_train, dataset_test = dataset['train'], dataset['test']
-    return dataset_train, dataset_test
+def samples_to_dataset(sample_tuple):
+    names = ["text", "label", "label_name"]
+    train_table = pa.Table.from_arrays(
+        [pa.array(sample_tuple[0]), pa.array(sample_tuple[1]), pa.array(list(map(str, sample_tuple[1])))], names=names)
+    train_dataset = Dataset(train_table)
+    val_table = pa.Table.from_arrays(
+        [pa.array(sample_tuple[2]), pa.array(sample_tuple[3]), pa.array(list(map(str, sample_tuple[3])))], names=names)
+    val_dataset = Dataset(val_table)
+    test_table = pa.Table.from_arrays(
+        [pa.array(sample_tuple[4]), pa.array(sample_tuple[5]), pa.array(list(map(str, sample_tuple[5])))], names=names)
+    test_dataset = Dataset(test_table)
+    return train_dataset, val_dataset, test_dataset
 
 
 if __name__ == '__main__':
-    positive_tweets, negative_tweets = load_twitter_corpus()
-    df = samples_to_df(positive_tweets, negative_tweets)
-    dataset_train, dataset_test = dataset_from_df(df)
-    print(dataset_train)
-    print(dataset_test)
-
+    samples = load_train_val_test_baseline('twitter')
+    train_samples, train_labels, val_samples, val_labels, test_samples, test_labels = samples
+    print(len(train_samples), len(val_samples), len(test_samples))
+    print(train_samples[0], train_labels[0])
+    train_dataset, val_dataset, test_dataset = samples_to_dataset(samples)
+    print(train_dataset, val_dataset, test_dataset)
