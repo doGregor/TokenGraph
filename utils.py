3532d181d8cc3a2afb6276172ed4d8593e16321c
Gregor Donabauer <gregor.donabauer@gmx.de>
2024-07-31 14:52:36 +0200

    control randomness ablation

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import pickle
 import os
 import sys
+import random
 from random import shuffle
+import torch_geometric.transforms as T
+import torch
 
 
 def save_graph(graph, file_name, dataset):
@@ -21,7 +24,22 @@ def get_saved_graphs(dataset):
     return [x for x in os.listdir('graphs/graphs_' + dataset) if x.endswith('pickle')]
 
 
-def load_train_eval_test(dataset, num_per_class_train=20, num_per_class_eval=20):
+def change_n_hop_neighborhood(graph, n_hops):
+    number_of_nodes = graph.x.shape[0]
+    edge_index = [[], []]
+    for i_from in range(0, number_of_nodes):
+        for i_to in range(i_from+1, i_from+n_hops):
+            if i_to < number_of_nodes:
+                edge_index[0].append(i_from)
+                edge_index[1].append(i_to)
+    graph.edge_index = torch.LongTensor(edge_index)
+    graph = T.RemoveDuplicatedEdges()(graph)
+    graph = T.ToUndirected()(graph)
+    return graph
+
+
+def load_train_eval_test(dataset, num_per_class_train=20, num_per_class_eval=20, n_hop_neighborhood=2,
+                         random_samples=True):
     if dataset == 'twitter':
         pos_samples = []
         neg_samples = []
@@ -33,8 +51,12 @@ def load_train_eval_test(dataset, num_per_class_train=20, num_per_class_eval=20)
                 neg_samples.append(graph)
             if graph_name[:3] == 'pos':
                 pos_samples.append(graph)
-        shuffle(pos_samples)
-        shuffle(neg_samples)
+        if random_samples:
+            shuffle(pos_samples)
+            shuffle(neg_samples)
+        else:
+            random.Random(42).shuffle(pos_samples)
+            random.Random(42).shuffle(neg_samples)
         train_graphs = pos_samples[:num_per_class_train] + neg_samples[:num_per_class_train]
         eval_graphs = (pos_samples[num_per_class_train:num_per_class_train+num_per_class_eval] +
                        neg_samples[num_per_class_train:num_per_class_train+num_per_class_eval])
@@ -51,8 +73,12 @@ def load_train_eval_test(dataset, num_per_class_train=20, num_per_class_eval=20)
                 neg_samples.append(graph)
             if graph_name[:3] == 'pos':
                 pos_samples.append(graph)
-        shuffle(pos_samples)
-        shuffle(neg_samples)
+        if random_samples:
+            shuffle(pos_samples)
+            shuffle(neg_samples)
+        else:
+            random.Random(42).shuffle(pos_samples)
+            random.Random(42).shuffle(neg_samples)
         train_graphs = pos_samples[:num_per_class_train] + neg_samples[:num_per_class_train]
         eval_graphs = (pos_samples[num_per_class_train:num_per_class_train + num_per_class_eval] +
                        neg_samples[num_per_class_train:num_per_class_train + num_per_class_eval])
@@ -78,7 +104,10 @@ def load_train_eval_test(dataset, num_per_class_train=20, num_per_class_eval=20)
         eval_graphs = []
         test_graphs = []
         for label in list(sample_dict.keys()):
-            shuffle(sample_dict[label])
+            if random_samples:
+                shuffle(sample_dict[label])
+            else:
+                random.Random(42).shuffle(sample_dict[label])
             train_graphs += sample_dict[label][:num_per_class_train]
             eval_graphs += sample_dict[label][num_per_class_train:num_per_class_train + num_per_class_eval]
             test_graphs += sample_dict[label][num_per_class_train + num_per_class_eval:]
@@ -101,14 +130,27 @@ def load_train_eval_test(dataset, num_per_class_train=20, num_per_class_eval=20)
         eval_graphs = []
         test_graphs = []
         for label in list(sample_dict.keys()):
-            shuffle(sample_dict[label])
+            if random_samples:
+                shuffle(sample_dict[label])
+            else:
+                random.Random(42).shuffle(sample_dict[label])
             train_graphs += sample_dict[label][:num_per_class_train]
             eval_graphs += sample_dict[label][num_per_class_train:num_per_class_train + num_per_class_eval]
             test_graphs += sample_dict[label][num_per_class_train + num_per_class_eval:]
 
-    shuffle(train_graphs)
-    shuffle(eval_graphs)
-    shuffle(test_graphs)
+    if n_hop_neighborhood != 3:
+        train_graphs = [change_n_hop_neighborhood(g, n_hop_neighborhood) for g in train_graphs]
+        eval_graphs = [change_n_hop_neighborhood(g, n_hop_neighborhood) for g in eval_graphs]
+        test_graphs = [change_n_hop_neighborhood(g, n_hop_neighborhood) for g in test_graphs]
+
+    if random_samples:
+        shuffle(train_graphs)
+        shuffle(eval_graphs)
+        shuffle(test_graphs)
+    else:
+        random.Random(42).shuffle(train_graphs)
+        random.Random(42).shuffle(eval_graphs)
+        random.Random(42).shuffle(test_graphs)
     return train_graphs, eval_graphs, test_graphs
 
 
